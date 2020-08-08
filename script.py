769b9645120679db24d8947846da433fdbb7c015
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-08-08 11:53:22 -0500

    added test objects

---
+++ script.py
@@ -0,0 +1,6 @@
+
+# List of destinations
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
+
+# Test traveler with name, trip and list of interests
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
