50e29c023f48d905a19b771829b8e49535ec707d
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-08-10 09:59:09 -0500

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,22 +1,46 @@
-
 # List of destinations
-destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
-
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+attractions = [ [] for val in destinations]
 # Test traveler with name, trip and list of interests
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def get_destination_index(destination):
-    destination_index = 0
-    for dest in destinations:
-        if dest == destination:
-            destination_index = destinations.index(destination)
-            return destination_index
+    try:
+        destination_index = destinations.index(destination)
+        return destination_index
+    except ValueError:
+        #print('error in get_destination_index function')
+        return 'error in get_destination_index function'
 
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index]
+        attractions_for_destination.append(attraction)
+        return attractions_for_destination
+    except ValueError:
+        return
+
 test_destination_index = get_traveler_location(test_traveler)
 
-print(test_destination_index)
+print('Destinations: ', destinations)
+print('Destination Index: ', get_destination_index('Los Angeles, USA'))
+print('Traveler Location: ', get_traveler_location(test_traveler))
+add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print('Attractions: ', attractions)
+#print(test_destination_index)
