e19f47d45a3a9df574270950acea531579cc48c1
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-08-11 10:49:16 -0500

    Added interest finder logic

--- script.py
+++ script.py
@@ -26,11 +26,19 @@ def add_attraction(destination, attraction):
     except ValueError:
         return
 
-test_destination_index = get_traveler_location(test_traveler)
+# Match traveler's interests with possible locations
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for possible_attraction in attractions_in_city:
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
 
-print('Destinations: ', destinations)
-print('Destination Index: ', get_destination_index('Los Angeles, USA'))
-print('Traveler Location: ', get_traveler_location(test_traveler))
+# Add Attractions
 add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -42,5 +50,17 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print('Attractions: ', attractions)
-#print(test_destination_index)
+
+test_destination_index = get_traveler_location(test_traveler)
+
+print('Destinations: ', destinations)
+print('Destination Index: ', get_destination_index('Los Angeles, USA'))
+print('Traveler Location: ', get_traveler_location(test_traveler))
+
+la_arts = find_attractions('Los Angeles, USA', ['art'])
+print(la_arts)
+
+
+print('Attractions List: ', attractions[2])
+
+
